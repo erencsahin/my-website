40fcfc9a3e721d4a2b47f3083dcac47e04769d99
Eren Can ŞAHİN <eren.sahin10@ogr.sakarya.edu.tr>
2023-08-04 19:19:32 +0300

    apiden hobiler için data çekildi

--- login.js
+++ login.js
@@ -1,4 +1,3 @@
-// app.js
 new Vue({
     el: '#iletisim',
     data: {
@@ -11,16 +10,11 @@ new Vue({
     },
     methods: {
         submitForm() {
-            // Form gönderildiğinde bu fonksiyon çalışacak
-            // Gerekli kontrol işlemlerini yapabilirsiniz
-
-            // Örneğin, e-posta formatını kontrol edebiliriz:
             if (!this.isValidEmail(this.formData.email)) {
                 return;
             }
 
             alert('Gönderildi,teşekkür ederiz.')
-            // Form verilerini başka bir sayfada görüntülemek için localStorage'e kaydediyoruz
             
             
         },
@@ -29,14 +23,12 @@ new Vue({
             window.location.href='veriler.html';
         },
         clearForm() {
-            // Formu temizleme işlemi
             this.formData.name = '';
             this.formData.surname='';
             this.formData.email = '';
             this.formData.message = '';
         },
         isValidEmail(email) {
-            // E-posta formatını kontrol eden fonksiyon
             const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
             return emailPattern.test(email);
         },
