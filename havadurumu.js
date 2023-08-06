447d4d28861947269e76c9b20bc33ec7d5ba73f4
Eren Can ŞAHİN <eren.sahin10@ogr.sakarya.edu.tr>
2023-08-06 21:46:56 +0300

    apiden online olarak şehrim sayfasına data çekildi. şehrimin takımı sayfasına giriş yapıldı

--- havadurumu.js
+++ havadurumu.js
@@ -1,2 +1,49 @@
-const url = 'https://api.openweathermap.org/data/2.5/weather?lat={40.7747}&lon={29.4008}&appid={16a31c8d080e80aee16040216a812275}'
-const key = "16a31c8d080e80aee16040216a812275";
+const API_KEY = "16a31c8d080e80aee16040216a812275";
+const CITY_NAME = "Darıca";
+
+const cityElement = document.querySelector(".city");
+const tempElement = document.querySelector(".temp");
+const descElement = document.querySelector(".desc");
+const minmaxElement = document.querySelector(".minmax");
+
+function displayWeatherData(data) {
+  const weatherDescription = data.weather[0].description;
+  const temperature = data.main.temp;
+  const minTemp = data.main.temp_min;
+  const maxTemp = data.main.temp_max;
+
+  cityElement.textContent = CITY_NAME;
+  tempElement.textContent = `${temperature} °C`;
+  descElement.textContent = weatherDescription;
+  minmaxElement.textContent = `Min: ${minTemp} °C, Max: ${maxTemp} °C`;
+}
+
+function getWeatherData() {
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${CITY_NAME}&lang=tr&appid=${API_KEY}&units=metric`;
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => displayWeatherData(data))
+    .catch(error => {
+      console.log("Hata:", error);
+      cityElement.textContent = "Hava durumu verileri alınamadı.";
+    });
+}
+/* function displayWeatherData(data) {
+    const weatherDescription = data.weather[0].description;
+    const temperatureKelvin = data.main.temp;
+    const minTempKelvin = data.main.temp_min;
+    const maxTempKelvin = data.main.temp_max;
+  
+    const temperatureCelsius = (temperatureKelvin - 273.15).toFixed(1);
+    const minTempCelsius = (minTempKelvin - 273.15).toFixed(1);
+    const maxTempCelsius = (maxTempKelvin - 273.15).toFixed(1);
+  
+    cityElement.textContent = CITY_NAME;
+    tempElement.textContent = `${temperatureCelsius} °C`;
+    descElement.textContent = weatherDescription;
+    minmaxElement.textContent = `Min: ${minTempCelsius} °C, Max: ${maxTempCelsius} °C`;
+  }
+   */
+
+getWeatherData();
